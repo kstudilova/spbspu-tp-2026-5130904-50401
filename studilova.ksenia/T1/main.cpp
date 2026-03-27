8afc47030a68c762515711229b0560189412083f
kstudilova <kstudilova@gmail.com>
2026-03-27 23:16:06 +0300

    add CLI

--- studilova.ksenia/T1/main.cpp
+++ studilova.ksenia/T1/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <unordered_map>
+#include <limits>
+
+#include "commands.hpp"
 
 int main()
 {
+  studilova::Context ctx;
+  std::unordered_map< std::string, studilova::cmd_t > cmds;
+  std::string cmd;
+
+  while (std::cin >> cmd)
+  {
+    try
+    {
+      cmds.at(cmd)(std::cin, std::cout, ctx);
+    }
+    catch(const std::out_of_range&)
+    {
+      std::cout << "<INVALID COMMAND>\n";
+
+      auto toignore = std::numeric_limits< std::streamsize >::max();
+      std::cin.ignore(toignore, '\n');
+    }
+    catch (...)
+    {
+      std::cout << "<INVALID COMMAND>\n";
+    }
+  }
+
   return 0;
 }
