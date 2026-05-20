c7be9dee74949ed082ab608527b23153cb62f18b
kstudilova <kstudilova@gmail.com>
2026-05-20 21:09:04 +0300

    stream operators for IO helpers

--- studilova.ksenia/T2/datastruct.cpp
+++ studilova.ksenia/T2/datastruct.cpp
@@ -1,5 +1,49 @@
 #include "datastruct.hpp"
 
+std::istream& studilova::operator>>(std::istream& in, DelimiterIO&& dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+
+  char c = '0';
+  in >> c;
+
+  if (in && c != dest.exp)
+  {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+
+std::istream& studilova::operator>>(std::istream& in, StringIO&& dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+  return std::getline( in >> DelimiterIO{ '"' }, dest.ref, '"');
+}
+
+std::istream& studilova::operator>>(std::istream& in, LabelIO&& dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+
+  std::string data = "";
+  if ((in >> StringIO{ data }) && (data != dest.exp))
+  {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+
 bool studilova::operator<(const DataStruct& lhs, const DataStruct& rhs)
 {
   if (lhs.key1 != rhs.key1)
