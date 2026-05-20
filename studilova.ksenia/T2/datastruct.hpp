e49ad5cfa5233cb2c29b332565d32f53e8be1108
kstudilova <kstudilova@gmail.com>
2026-05-20 20:41:32 +0300

    add IO helpers Delimiter, String, Label

--- studilova.ksenia/T2/datastruct.hpp
+++ studilova.ksenia/T2/datastruct.hpp
@@ -12,6 +12,25 @@ namespace studilova
     std::string key3;
   };
 
+  struct DelimiterIO
+  {
+    char exp;
+  };
+
+  struct StringIO
+  {
+    std::string& ref;
+  };
+
+  struct LabelIO
+  {
+    std::string exp;
+  };
+
+  std::istream& operator>>(std::istream& in, DelimiterIO&& dest);
+  std::istream& operator>>(std::istream& in, StringIO&& dest);
+  std::istream& operator>>(std::istream& in, LabelIO&& dest);
+
   bool operator<(const DataStruct& lhs, const DataStruct& rhs);
 }
 
