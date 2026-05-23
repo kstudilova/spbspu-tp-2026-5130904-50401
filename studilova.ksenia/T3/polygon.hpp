5f71e1e5f01685eaa43cdf302496b4487c5612a4
kstudilova <kstudilova@gmail.com>
2026-05-23 14:21:41 +0300

    add polygon structures

---
+++ studilova.ksenia/T3/polygon.hpp
@@ -0,0 +1,47 @@
+#ifndef POLYGON_HPP
+#define POLYGON_HPP
+
+#include <iostream>
+#include <vector>
+
+namespace studilova
+{
+  struct Point
+  {
+    int x;
+    int y;
+  };
+
+  struct Polygon
+  {
+    std::vector< Point > points;
+  };
+
+  struct Triangle
+  {
+    Point a;
+    Point b;
+    Point c;
+  };
+
+  struct DelimiterIO
+  {
+    char exp;
+  };
+
+  class IOGuard
+  {
+    public:
+      explicit IOGuard(std::basic_ios< char >& s);
+      ~IOGuard();
+
+    private:
+      std::basic_ios< char >& s_;
+      std::streamsize width_;
+      std::streamsize precision_;
+      std::basic_ios< char >::fmtflags flags_;
+      char fill_;
+  };
+}
+
+#endif
